50f47289f36f046cbe86e10b2eba487f09184adc
ymikawa <yukimikawa@gmail.com>
2016-01-28 17:24:03 -0800

    Checkpoint 34 topics and posts: completed

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
  # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
  gem 'rails', '4.2.5'
+ gem 'shoulda'
 
  group :production do
    gem 'pg'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -15,5 +25,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
